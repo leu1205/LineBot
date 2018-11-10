ba47ede22a42435818c0f8fce058518c788443bf
leu1205 <leu1205@gmail.com>
2018-11-11 01:34:15 +0800

    push

--- route.js
+++ route.js
@@ -5,20 +5,21 @@ const router = Router();
 router.post('/linewebhook',async function(ctx, next){
     const channelSecret = '377bbe253460bbd5ec813237a166bebe';
     const KoaRequest = ctx.request;
-    const replyToken = KoaRequest.params['events'][0]['replyToken'];
+    const json = JSON.stringify(KoaRequest.body);
+    //const replyToken = json['events'][0]['replyToken'];
     const hash = crypto.createHmac('sha256', channelSecret)
-              .update(Buffer.from(JSON.stringify(KoaRequest.body), 'utf8'))
+              .update(Buffer.from(json, 'utf8'))
               .digest('base64');
-    console.log(KoaRequest.body);
+    console.log(json);
     if(KoaRequest.headers['x-line-signature'] === hash){
         ctx.status = 200;
     }else{
-        if(replyToken === '00000000000000000000000000000000' || replyToken === 'ffffffffffffffffffffffffffffffff'){
+        /*if(replyToken === '00000000000000000000000000000000' || replyToken === 'ffffffffffffffffffffffffffffffff'){
             ctx.status = 200;
-        }else{
+        }else{*/
             ctx.body = 'Unauthorized!';
             ctx.status = 401;
-        }
+        //}
     }
 
     await next();
