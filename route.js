f94e4c65f43d22a9b94e3ed187dda943fcdfe7b0
leu1205 <leu1205@gmail.com>
2018-11-10 23:00:37 +0800

    fix

--- route.js
+++ route.js
@@ -4,15 +4,15 @@ const router = Router();
 
 router.post('/linewebhook',async function(ctx, next){
     const channelSecret = '377bbe253460bbd5ec813237a166bebe';
-    const hash = crypto.createHmac('sha256', channelSecret)
+    const hash = await crypto.createHmac('sha256', channelSecret)
         .update(Buffer.from(JSON.stringify(ctx.request.body), 'utf-8')).digest('base64');
     
-        if(ctx.request.headers['x-line-signature'] === hash){
-            ctx.status = 200;
-        }else{
-            ctx.body = 'Unauthorized!';
-            ctx.status = 401;
-        }
+    if(ctx.request.headers['x-line-signature'] === hash){
+        ctx.status = 200;
+    }else{
+        ctx.body = 'Unauthorized!';
+        ctx.status = 401;
+    }
 
     await next();
 });
