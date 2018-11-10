13c3af9ccf879b40d9f2316427d3e2775769ea96
leu1205 <leu1205@gmail.com>
2018-11-11 03:50:31 +0800

    push

--- route.js
+++ route.js
@@ -2,12 +2,17 @@ const Router = require('koa-router');
 const crypto = require('crypto');
 const router = Router();
 
+router.post('/',function(){
+    ctx.body = "Connected"
+})
+
 router.post('/linewebhook',async function(ctx, next){
     const channelSecret = '377bbe253460bbd5ec813237a166bebe';
     const KoaRequest = ctx.request;
+    const requestEvents = koaRequest.body.events;
     const json = JSON.stringify(KoaRequest.body);
     console.log(json);
-    const replyToken = json.events[0].replyToken;
+    //const replyToken = json.events.replyToken;
     const hash = crypto.createHmac('sha256', channelSecret)
               .update(Buffer.from(json, 'utf8'))
               .digest('base64');
@@ -15,12 +20,12 @@ router.post('/linewebhook',async function(ctx, next){
     if(KoaRequest.headers['x-line-signature'] === hash){
         ctx.status = 200;
     }else{
-        if(replyToken === '00000000000000000000000000000000' || replyToken === 'ffffffffffffffffffffffffffffffff'){
+        /*if(replyToken === '00000000000000000000000000000000' || replyToken === 'ffffffffffffffffffffffffffffffff'){
             ctx.status = 200;
-        }else{
+        }else{*/
             ctx.body = 'Unauthorized!';
             ctx.status = 401;
-        }
+        //}
     }
 
     await next();
