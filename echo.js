5c900c848c22d2f0f791e819d3f151c83740d6d3
leu1205 <leu1205@gmail.com>
2018-11-13 04:53:38 +0800

    added some conversation.

--- echo.js
+++ echo.js
@@ -1,16 +1,54 @@
 module.exports = (bot) => {
-    return async (ctx,next) => {
+    return async (ctx, next) => {
         bot.parse(ctx.request.body);
-        
-        bot.on('follow',function(event){
-            event.push(event.source.userId,{type:'text', text:"感謝你願意當我的朋友QAQ"});
+
+        bot.on('follow', function (event) {
+            event.push(event.source.userId, {
+                type: 'text',
+                text: "感謝你願意當我的朋友QAQ"
+            });
         });
-        bot.on('message',function(event){
-            if(event.message.text == "誰長得像金城武"){
-                event.reply({type:'text', text: "當然4你啊"});
-            }else{
-                event.reply(event.message.text);
+
+        bot.on('message', function (event) {
+            if (event.message.text == "誰長得像金城武") {
+                event.reply({
+                    type: 'text',
+                    text: "當然4你啊"
+                });
+            } else {
+                event.reply(replyMessage(event.message));
             }
         });
+    }
+}
+
+function replyMessage(message) {
+    switch (message.type) {
+        case 'text':
+            return {
+                type: 'text',
+                text: message.text
+            }
+        case 'image':
+            return {
+                type: 'text',
+                text: "又傳什麼色色的圖片給我啊"
+            }
+        case 'file':
+            return {
+                type: 'text',
+                text: "這..有毒嗎?別把我這當雲端空間啊!"
+            }
+        case 'sticker':
+            return {
+                type: 'sticker',
+                packageId: message.packageId,
+                stickerId: message.stickerId
+            }
+        default:
+            return {
+                type: 'text',
+                text: "恩恩"
+            }
     }
 }
