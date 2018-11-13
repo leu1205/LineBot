0a29d65e2e811cd7fb7695767f87db5e0e80bc63
leu1205 <leu1205@gmail.com>
2018-11-13 21:59:15 +0800

    untracked

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = config = {
-    channelId: '1621025919',
-    channelSecret: '377bbe253460bbd5ec813237a166bebe',
-    channelAccessToken: 'BkGgYyosidevF8qOEOIGUYSAyGijGa+HQ93cG0Gc8VjLNop6OPiDl1zVKsFjSU6nmGwkbKroMjzafFctpi8+zPCxTT0HljjsVP7IYOF/iwx93RoE+uOrCFto48P4jbNPjRIIXVIQs5MkwY1XLKOHoAdB04t89/1O/w1cDnyilFU='
-};
