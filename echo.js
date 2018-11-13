2a8ea267f2f3ed46c5007c901a5c99645377ce35
leu1205 <leu1205@gmail.com>
2018-11-13 21:28:11 +0800

    fixed

--- echo.js
+++ echo.js
@@ -13,6 +13,10 @@ module.exports = (bot) => {
 
         bot.on('message', async function (event) {
             if (event.message.text == "日幣") {
+                event.reply({
+                    type: 'text',
+                    text: "你等等，我馬上幫你查。"
+                });
                 let msg = await Best_3();
                 event.reply({
                     type: 'text',
@@ -27,7 +31,7 @@ module.exports = (bot) => {
 
 async function Best_3(){
     let JPY = await BestRate_JPY();
-    return JPY[0].join(":")+"\n"+JPY[1].join(":")+"\n"+JPY[2].join(":");
+    return "目前日幣最佳前三匯率：\n"+JPY[0].join("：")+"\n"+JPY[1].join("：")+"\n"+JPY[2].join("：");
 }
 
 function replyMessage(message) {
